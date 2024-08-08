5d049729d054b8b23d46d0aed41e1984b9b4a416
Jason Cai <jasoncai528@gmail.com>
2024-08-08 10:53:44 -0700

    tasks page and ui

---
+++ Models/WorkItem.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleBlazorApp.Models
+{
+    public class WorkItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TaskStatus Status { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime? CompletedDateTime { get; set; }
+    }
+
+    public enum TaskStatus
+    {
+        New,
+        InProgress,
+        Complete
+    }
+}
