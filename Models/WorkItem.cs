6f2b8e53509ed96ef39cd225a7438856b27177c8
Jason Cai <jasoncai528@gmail.com>
2024-08-09 12:02:19 -0700

    new task item form

--- Models/WorkItem.cs
+++ Models/WorkItem.cs
@@ -1,8 +1,11 @@
-﻿namespace SimpleBlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlazorApp.Models
 {
     public class WorkItem
     {
         public string Id { get; set; }
+        [Required(ErrorMessage = "Task Name is required")]
         public string Name { get; set; }
         public string Description { get; set; }
         public TaskStatus Status { get; set; }
